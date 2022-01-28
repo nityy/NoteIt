f97ad3c677b7207285567d16a62e50fc4e0d04a6
Nitish Yadav <nitishy01@gmail.com>
2022-01-28 10:36:41 +0530

    add create note

--- notes/urls.py
+++ notes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from notes.views import NoteDetailView, NoteListView
+from notes.views import NoteDetailView, NoteListView, NoteAddView
 
 app_name = "notes"
 urlpatterns = [
     path('', NoteListView.as_view(), name='list'),
-    path('<int:pk>/', NoteDetailView.as_view(), name='detail')
+    path('<int:pk>/', NoteDetailView.as_view(), name='detail'),
+    path('new/', NoteAddView.as_view(), name='add')
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from notes.models import Note
+from django.shortcuts import render
+from notes.forms import NoteForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -10,3 +14,15 @@ class NoteListView(ListView):
 
 class NoteDetailView(DetailView):
     model = Note
+
+
+class NoteAddView(View):
+    def get(self, request):
+        f = NoteForm()
+        return render(request, 'notes/note_new.html', {'form': f})
+
+    def post(self, request):
+        f = NoteForm(request.POST)
+        if f.is_valid():
+            f.save()
+            return HttpResponseRedirect(reverse("notes:list"))
