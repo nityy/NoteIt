5afdb13844a44c0ef7acd26a0efc2e4a6b562b9c
Nitish Yadav <nitishy01@gmail.com>
2022-01-29 00:14:51 +0530

    enforce note title to be longe than single char

--- notes/models.py
+++ notes/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
 class Note(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, validators=[
+                             MinLengthValidator(2, "Title must be longer than 1 character")])
     body = models.CharField(max_length=1000)
 
     def __str__(self):
