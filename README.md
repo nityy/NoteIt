6ad25116d80c1ae53afd9b8ace3546c513b8a5a2
Nitish Yadav <nitishy01@gmail.com>
2022-01-30 10:32:43 +0530

    add support for linebreaks in note

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ A simple, personal notes app written in Python. Currently single-user.
 
 ## Roadmap
 
+- Markdown
 - Multiple users
 - Tags, colors
--- notes/models.py
+++ notes/models.py
@@ -6,8 +6,7 @@ from django.urls import reverse_lazy
 class Note(models.Model):
     title = models.CharField(max_length=100, validators=[
                              MinLengthValidator(2, "Title must be longer than 1 character")], help_text="Enter title of the note")
-    body = models.CharField(
-        max_length=1000, help_text="Enter body of the note")
+    body = models.TextField(help_text="Enter body of the note")
     link = models.URLField(
         blank=True, validators=[URLValidator(schemes=['http', 'https'])], error_messages={'invalid': 'URL must start with http or https'}, help_text="Enter an optional URL beginning with http or https")
 
